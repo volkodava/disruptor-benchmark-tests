5d49925a6b21288db730595ad7c6b054c9130c17
Anatolii Volkodav <volkodavav@gmail.com>
2016-05-16 12:10:16 +0200

    updated benchmark config

--- src/main/java/com/disruptor/benchmark/Configuration.java
+++ src/main/java/com/disruptor/benchmark/Configuration.java
@@ -2,7 +2,7 @@ package com.disruptor.benchmark;
 
 public class Configuration {
     public static final int FORKS = 1;
-    public static final int WARMUP_ITERATIONS = 5;
+    public static final int WARMUP_ITERATIONS = 10;
     public static final int MEASUREMENT_ITERATIONS = 10;
     public static final long VALUE = 123456789L;
 }
